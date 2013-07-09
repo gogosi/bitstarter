6871ea86cf59c1bf772f34bc4e2892fc9576d942
Si Bunting <si@goongoout.com>
2013-07-09 10:45:19 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,8 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var indexBuffer = fs.readFile('index.html');
+
+var index = buf.toString(indexBuffer);
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(index);
 });
 
 var port = process.env.PORT || 5000;
